4ac3a4ac69d8d697780490685e3d17bfa648a5bd
ReduceUI <115711792+ReduceUI@users.noreply.github.com>
2025-08-20 17:44:35 +1000

    progressing Fragments

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment : Fragment() {
+    private lateinit var crime : Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+}
